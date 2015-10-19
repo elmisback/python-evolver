f1a9a573b95e4681e809a094aca3cadccc5c75a3
ulmusic <elmisback@gmail.com>
2015-10-19 11:48:34 -0400

    finished packaging the library

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='evolver',
+      version='0.1',
+      description='Python wrapper for Brakke\'s surface evolver',
+      url='http://github.com/ulmusic/python-evolver',
+      author='Edward Misback',
+      author_email='elmisback@gmail.com',
+      license='GNU GPL',
+      packages=['evolver'],
+      zip_safe=False)
